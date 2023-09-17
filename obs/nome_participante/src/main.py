2073c8e52bb7e4ff519bea9321d39fcade05a543
Leandro Zirondi <zirondil@gmail.com>
2023-09-16 23:04:31 -0300

    Adição do template para a direita.

--- obs/nome_participante/src/main.py
+++ obs/nome_participante/src/main.py
@@ -18,43 +18,57 @@ ICON_INFO = 0x40
 # consts
 
 FNAME_DATA = 'data.json'
-FNAME_TEMPLATE = 'Template.html'
+FNAME_TEMPLATE_DEFAULT = 'Template.html'
+FNAME_TEMPLATE_DIREITA = 'Template-Direita.html'
 
 # %%
 # functions
 
-def validate_files():
+def validate_files() -> None:
     if not os.path.isfile(FNAME_DATA):
         ctypes.windll.user32.MessageBoxW(0, "Arquivo data.json nao econtrado.", "Erro", ICON_EXCLAIM | MB_OK)
         sys.exit(1)
 
-    if not os.path.isfile(FNAME_TEMPLATE):
-        ctypes.windll.user32.MessageBoxW(0, "Arquivo Template.html nao econtrado.", "Erro", ICON_EXCLAIM | MB_OK)
+    if not os.path.isfile(FNAME_TEMPLATE_DEFAULT):
+        ctypes.windll.user32.MessageBoxW(0, f"Arquivo {FNAME_TEMPLATE_DEFAULT} nao econtrado.", "Erro", ICON_EXCLAIM | MB_OK)
+        sys.exit(1)
+
+    if not os.path.isfile(FNAME_TEMPLATE_DIREITA):
+        ctypes.windll.user32.MessageBoxW(0, f"Arquivo {FNAME_TEMPLATE_DIREITA} nao econtrado.", "Erro", ICON_EXCLAIM | MB_OK)
         sys.exit(1)
 
 def get_htmls() -> list:
-    with open(FNAME_TEMPLATE, 'r') as f:
-        html = f.read()
-    return html.split('<!-- SPLIT -->')
+    with open(FNAME_TEMPLATE_DEFAULT, 'r') as f:
+        html_default = f.read()
     
-def make_html(html_upper: str, html_lower: str):
+    with open(FNAME_TEMPLATE_DIREITA, 'r') as f:
+        html_direita = f.read()
+    
+    return html_default.split('<!-- SPLIT -->'), html_direita.split('<!-- SPLIT -->')
+    
+def make_html(htmls: list) -> None:
     with open(FNAME_DATA, 'r') as f:
         data = json.load(f)
     
     for key in data:
-        with open(key, 'w') as f:
-            f.write(html_upper)
-            f.write(data[key])
-            f.write(html_lower)
+        if not 'Direita' in key:
+            with open(key, 'w') as f:
+                f.write(htmls[0][0])
+                f.write(data[key])
+                f.write(htmls[0][1])
+        else:
+            with open(key, 'w') as f:
+                f.write(htmls[1][0])
+                f.write(data[key])
+                f.write(htmls[1][1])
     
     ctypes.windll.user32.MessageBoxW(0, f"Arquivo(s) alterados com sucesso!", u"Sucesso!", ICON_INFO | MB_OK)
-    
+
 # %%
 # main
 if __name__ == '__main__':
     validate_files()
-    html_upper, html_lower = get_htmls()
-    make_html(html_upper, html_lower)
-    
+    htmls = get_htmls()
+    make_html(htmls)
 
 
